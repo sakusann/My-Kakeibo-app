9dd83f4737a5b0106bf78086d18fb8f0b166a80c
sakusann <38159300+sakusann@users.noreply.github.com>
2025-08-12 21:46:25 +0900

    a

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      root: './frontend',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, './frontend/src'), // Point to frontend/src
+          '@': './src', // Point to frontend/src
         }
       }
     };
